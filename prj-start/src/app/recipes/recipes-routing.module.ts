9955394ac964c52b7ec7687feb78b040a409e541
Braulio Alejandro Garcia Rivera <braulio.garcia@enkoder.com.mx>
2023-02-13 09:30:57 -0600

    last update

--- prj-start/src/app/recipes/recipes-routing.module.ts
+++ prj-start/src/app/recipes/recipes-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes.component';
 import { AuthGuard } from '../auth/auth.guard';
-import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeStartComponent } from './recipe-list/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesResolverService } from './recipes-resolver.service';
--- prj-start/src/app/recipes/recipes.module.ts
+++ prj-start/src/app/recipes/recipes.module.ts
@@ -6,7 +6,7 @@ import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeStartComponent } from './recipe-list/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { SharedModule } from '../shared/shared.module';
